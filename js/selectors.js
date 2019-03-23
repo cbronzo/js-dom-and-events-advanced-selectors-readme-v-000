2cc36b2c05dd3277db54756aaa90429941b0b9a8
Catherine Bronzo <cbronzo@mac.com>
2019-03-23 22:17:02 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -12,3 +12,10 @@ function lastImageSelector() {
 function ninjaBabySelector() {
     return $('#baby-ninja');
 }
+
+function divSelector() {
+  return $('div.pics')
+}
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
